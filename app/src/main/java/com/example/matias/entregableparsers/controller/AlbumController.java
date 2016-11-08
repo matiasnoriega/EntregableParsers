39687237ecbf503d603cc580151c8e751470f0dd
Mobile Turno Noche <dh-mob-tv@DH-A1-17.digitalhouse.local>
2016-11-07 22:02:55 -0300

    Ajustes generales en los XML y creacion de la innerclass del listener en AlbumController.java

--- app/src/main/java/com/example/matias/entregableparsers/controller/AlbumController.java
+++ app/src/main/java/com/example/matias/entregableparsers/controller/AlbumController.java
@@ -12,25 +12,33 @@ import java.util.List;
 
 
 public class AlbumController {
+    private ResultListener<List<Album>> resultListenerFromView;
+    private Context context;
 
     public void getAllAlbums(final Context context, final ResultListener<List<Album>> resultListenerFromView) {
 
         AlbumDAO albumDAO = new AlbumDAO(context);
+        this.resultListenerFromView = resultListenerFromView;
+        this.context = context;
 
         if(HTTPConnectionManager.isNetworkingOnline(context)) {
-            albumDAO.getAllPostFromInternet(new ResultListener<List<Album>>() {
-                @Override
-                public void finish(List<Album> resultado) {
-                    //QUE HACE EL CONTROLLER CUANDO RECIBE LA LISTA DE POST
-                    resultListenerFromView.finish(resultado);
-                    Toast.makeText(context, "Internet", Toast.LENGTH_SHORT).show();
-                }
-            });
+            albumDAO.getAllPostFromInternet(new ResultListenerController<List<Album>>());
         }
         else{
             List<Album> postList = albumDAO.getAllPostFromDatabase();
             resultListenerFromView.finish(postList);
             Toast.makeText(context, "Base de datos", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private class ResultListenerController<T> implements ResultListener<List<Album>> {
+
+        @Override
+        public void finish(List<Album> resultado) {
+
+            resultListenerFromView.finish(resultado);
+            Toast.makeText(context, "Internet", Toast.LENGTH_SHORT).show();
+
         }
     }
 }
